701ffcbb8dbbbcd6613f63a4fce28b6e9e5ccdb5
Laura Martin <surminus@gmail.com>
2018-09-10 14:18:18 +0100

    Allow tests to run
    When WebMock is invoked by other tests it causes an issue since we're
    not stubbing anything with WebMock in the basic exception test.
    
    This definitely needs to be improved, but I wish to get the exception
    handling change out.

--- spec/drone_autoscale_spec.rb
+++ spec/drone_autoscale_spec.rb
@@ -1,17 +1,22 @@
 require 'spec_helper'
+require 'webmock/rspec'
 
 require 'drone_autoscale'
 
 RSpec.describe DroneAutoScale do
   describe '#agent' do
     it 'should exit when endpoints are unavailable' do
+      WebMock.allow_net_connect!
       expect { described_class.start(['agent']) }.to raise_exception(SystemExit)
+      WebMock.disable_net_connect!
     end
   end
 
   describe '#server' do
     it 'should exit when endpoints are unavailable' do
+      WebMock.allow_net_connect!
       expect { described_class.start(['server']) }.to raise_exception(SystemExit)
+      WebMock.disable_net_connect!
     end
   end
 end
